8ad15e1108714b68da3b70fb9f635bb485cef7cf
satyaandm <43974317+SatyaanM@users.noreply.github.com>
2021-03-09 16:37:28 -0400

    Update problem13.py

--- problem13.py
+++ problem13.py
@@ -1,3 +1,4 @@
+#efficient method
 def romanToInt(self, s: str) -> int:
     dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
@@ -10,4 +11,40 @@ def romanToInt(self, s: str) -> int:
     return num
 
 
-print(romanToInt('', "LXVIII"))
+
+#inefficient method
+#     def romanToInt(self, s: str) -> int:
+#         dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+#         num = 0
+#         x = 0
+#         while x < len(s)-1:
+#             if s[x] == 'I':
+#                 if s[x + 1] == 'V' or s[x + 1] == 'X':
+#                     num += (dictionary[s[x + 1]] - dictionary[s[x]])
+#                     x += 2
+#                 else:
+#                     num += 1
+#                     x += 1
+#             elif s[x] == 'X':
+#                 if s[x + 1] == 'L' or s[x + 1] == 'C':
+#                     num += (dictionary[s[x + 1]] - dictionary[s[x]])
+#                     x += 2
+#                 else:
+#                     num += 10
+#                     x += 1
+#             elif s[x] == 'C':
+#                 if s[x + 1] == 'D' or s[x + 1] == 'M':
+#                     num += (dictionary[s[x + 1]] - dictionary[s[x]])
+#                     x += 2
+#                 else:
+#                     num += 100
+#                     x += 1
+#             else:
+#                 num += dictionary[s[x]]
+#                 x += 1
+#             print(num)
+#         if x == len(s)-1:
+#             num += dictionary[s[x]]
+#             x += 1
+#             print(num)
+#         return num
