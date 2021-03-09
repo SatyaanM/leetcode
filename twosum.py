1ad23b49560438b9990ab6d296e80331382cdcd7
satyaandm <satyaandm@gmail.com>
2021-03-09 14:39:59 -0400

    1. Two Sum

---
+++ twosum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        list = []
+        for x in range(len(nums)):
+            for y in range(len(nums)):
+                if (nums[x] == nums[y] and x == y):
+                    continue
+                else:
+                    if (nums[x] + nums[y] == target):
+                        list.append(x)
+                        list.append(y)
+                        return list
