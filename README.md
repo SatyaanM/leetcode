f0add0b5f37b4b63c3c5956a6ecc2a4345c4acf3
satyaandm <satyaandm@gmail.com>
2021-03-10 13:50:53 -0400

    1672. Richest Customer Wealth

--- README.md
+++ README.md
@@ -42,3 +42,6 @@ https://leetcode.com/problems/shuffle-the-array/
 
 ## 1480. Running Sum of 1d Array
 https://leetcode.com/problems/running-sum-of-1d-array/
+
+## 1672. Richest Customer Wealth
+https://leetcode.com/problems/richest-customer-wealth/
---
+++ problem1672.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        highest = 0
+        for customer in accounts:
+            sum = 0
+            for bank in customer:
+                sum += bank
+            if sum > highest:
+                highest = sum
+        return highest
